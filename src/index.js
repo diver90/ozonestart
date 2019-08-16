f1e7143969e5a7fb3a0496d016be187ae68006a5
diver90 <diver90.deep@gmail.com>
2019-08-16 11:20:00 +0300

    Search

--- src/index.js
+++ src/index.js
@@ -87,7 +87,11 @@ function addCart() {
 
 function actionPage() {
     const cards = document.querySelectorAll('.goods .card'),
-        discountCheckbox = document.getElementById('discount-checkbox');
+        discountCheckbox = document.getElementById('discount-checkbox'),
+        min = document.getElementById('min'),
+        max = document.getElementById('max'),
+        search = document.querySelector('.search-wrapper_input'),
+        searchBtn = document.querySelector('.search-btn');
     discountCheckbox.addEventListener('click', () => {
         cards.forEach((card) => {
             if (discountCheckbox.checked) {
@@ -98,6 +102,34 @@ function actionPage() {
                 card.parentNode.style.display = '';
             }
         });
+    });
+
+    function filterPrice() {
+        cards.forEach((card) => {
+            const cardPrice = card.querySelector('.card-price'),
+                price = parseFloat(cardPrice.textContent);
+
+                if  (( min.value && price < min.value ) || (min.value && price > max.value)){
+                    card.parentNode.style.display = 'none';                           
+            } else {
+                card.parentNode.style.display = '';              
+            }                
+        });
+    }
+    min.addEventListener('change', filterPrice);
+    max.addEventListener('change', filterPrice);
+
+    searchBtn.addEventListener('click', () => {
+        const searchText = new RegExp(search.value.trim(), 'i');
+        cards.forEach((card) => {
+            const title = card.querySelector('.card-title');
+            if (!searchText.test(title.textContent)){
+                card.parentNode.style.display = 'none';
+            } else {
+                card.parentNode.style.display = '';
+            }
+        });
+        search.value = '';
     });
 }
 
